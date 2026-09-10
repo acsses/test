9d7b6690577933093ae9668d884a9fbc41e97758
Eagle　Code <41446048+acsses@users.noreply.github.com>
2026-09-10 15:03:33 +0900

    Enhance addon function for multi-step request handling
    Refactor addon function to perform multiple requests with XSRF token handling.

--- index.js
+++ index.js
@@ -1,4 +1,53 @@
-export function addon(command, msg) {
-    console.log(`addon: ${command}`);
-    msg.reply("hello world! from addon")
+export async function addon(command, msg) {
+    const command1 = {
+        action: "test",
+        url: "https://event.yomiuri.co.jp/jssa/form/tokyo",
+        cookies: [],
+        body: {},
+        method: "GET",
+        header: {},
+    }
+    const res = await this.req(command1,msg)
+
+    var root = HTMLParser.parse(res.body);
+
+    var xsrf_token = root.querySelectorAll('input[name="_token"]')[0]?.attributes.value
+
+    var body = command.body
+
+    body["_token"]=xsrf_token
+
+    const command2 = {
+        action: "test",
+        url: "https://event.yomiuri.co.jp/jssa/form/tokyo/confirm",
+        cookies: [],
+        body: body,
+        method: "POST",
+        body_encode: "multipart",
+        header: {}
+    }
+
+    const res2 = await this.req(command2,msg)
+
+    var root2 = HTMLParser.parse(res2.body);
+
+    var token = root2.querySelectorAll('input[name="_token"]')[0]?.attributes.value
+
+    const command3 = {
+        action: "test",
+        url: "https://event.yomiuri.co.jp/jssa/form/tokyo",
+        cookies: [],
+        body: {
+            "_token":token
+        },
+        method: "POST",
+        body_encode: "multipart",
+        header: {}
+    }
+
+    const res3 = await this.req(command3,msg)
+
+
+
+
 }
